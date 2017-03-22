f643b1dd7ceefc0eea870f1c789c8c057cc598bf
Eric Schwartz <ericschwartz7@gmail.com>
2017-03-22 16:40:58 -0400

    add fork logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import fork from '../fork.jpg';
 import '../css/App.css';
 
 class App extends Component {
@@ -7,8 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={fork} className="App-logo" alt="logo" />
+          <h2>Welcome to Forking Paths</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
